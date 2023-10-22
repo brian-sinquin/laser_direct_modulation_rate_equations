f7f5373bf6b4a945e7084a3bb088e63eb3da55aa
Brian SINQUIN <brian.sinquin@gmail.com>
2023-10-22 20:23:48 +0200

    Small-signal transfer function update 🔧

--- laser_rate_equations.jl
+++ laser_rate_equations.jl
@@ -4,8 +4,8 @@ function laser!(du, u, p, s)
 
     (r, m, η, ε, α) = p # parameters
 
-    e = u[1]
-    n = u[2] # variables
+    e = u[1] # optical field
+    n = u[2] # carrier inversion
 
     Jm = r * (1 + m * cos(2π * η * s))
     J = Jm * (Jm >= 0) # clamping  # DC + AC pump must be null or positive (a laser is a diode)
@@ -20,15 +20,15 @@ function integrate(p, ta, tb, Np)
 
     r, m, f0, τp, τc, α = p
 
-    η = f0 * τp
-    ε = τp / τc
+    η = f0 * τp # normalized frequency
+    ε = τp / τc # lifetime ratio
 
     p0 = (r, m, η, ε, α)
 
     X0 = [0.000001 + 0.000001im, 0.000001] # initial condition 
     s = LinRange(ta, tb, Np) ./ τp
     prob = ODEProblem(laser!, X0, (0.0, s[end]), p0)
-    sol = solve(prob, Tsit5())
+    sol = solve(prob, RK4(), saveat=(tb - ta) / τp / Np)
     sol = sol(s)
     t = s .* τp
     E = sol[1, :]
